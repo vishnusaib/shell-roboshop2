e94c48e5a6db9b7ebd95eb612e5971816f9ccfb2
vishnusaib <sri.vishnusai9@gmail.com>
2026-05-03 11:24:46 +0530

    roboshop2

--- roboshop.sh
+++ roboshop.sh
@@ -3,13 +3,13 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0faecd55d9a7833d9"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID=Z06694613IPRQJX0M1H4B
+ZONE_ID="Z06694613IPRQJX0M1H4B"
 DOMAIN_NAME="vishnuv8.shop"
 
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0faecd55d9a7833d9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != frontend ]
+    if [ $instance != "frontend" ]
      then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
